139cfa01b0436a2f4b3e27a3f3dd69fd4b070f65
Wessel Poelman <contact+github@wesselpoelman.nl>
2026-02-09 14:05:28 +0100

    add ty in pre-commit, ignore old file temporarily

--- scripts/test_dump.py
+++ scripts/test_dump.py
@@ -1,15 +1,16 @@
-from pathlib import Path
+# TODO: once rebuild is complete
+# from pathlib import Path
 
-from qq import LanguageData
-
-
-def main():
-    path = Path(__file__).parent / "test.db"
-    ld = LanguageData.from_raw()
-    ld.dump(path)
-    LanguageData.from_db(path)
-    path.unlink()
+# from qq import LanguageData
 
 
-if __name__ == "__main__":
-    main()
+# def main():
+#     path = Path(__file__).parent / "test.db"
+#     ld = LanguageData.from_raw()
+#     ld.dump(path)
+#     LanguageData.from_db(path)
+#     path.unlink()
+
+
+# if __name__ == "__main__":
+#     main()
